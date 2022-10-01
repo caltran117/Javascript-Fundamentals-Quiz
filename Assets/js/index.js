1eaef9647c9fe56dae7726a655af4ff2222cd0d1
Calvin <84696281+caltran117@users.noreply.github.com>
2022-10-01 10:22:03 -0400

    added pseudo comments, start button created and event listener

---
+++ Assets/js/index.js
@@ -0,0 +1,21 @@
+// function startQuiz (){
+//     console.log("startquiz")
+// }
+
+// start button appears
+var startquizbutton = document.querySelector("#button");
+startquizbutton.addEventListener("click", function(event) {
+    console.log("startquiz")
+// button disapears, 
+// quiz starts-- see first questions & answers, 
+// timer starts, 
+// clicks an a answer/button then goes to next
+// end of 5 questions highscore chart will appear
+  });
+
+// storing questions & answers
+// 5 questions that will appear with answers
+
+// storing scores 
+// highscore chart ranks scores for high to low
+// scores of each question is kept tracked of
