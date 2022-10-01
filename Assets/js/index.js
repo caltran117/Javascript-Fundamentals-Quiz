fdfec8a83f8f6037bf83134b53ff144465c9e194
Calvin Tran <caltran@Calvins-MBP.fios-router.home>
2022-10-01 19:32:01 -0400

    added some pseudo comments

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,16 +2,21 @@
 //     console.log("startquiz")
 // }
 
-// start button appears
-var startquizbutton = document.querySelector("#button");
-startquizbutton.addEventListener("click", function(event) {
-    console.log("startquiz")
-// button disapears, 
-// quiz starts-- see first questions & answers, 
-// timer starts, 
-// clicks an a answer/button then goes to next
-// end of 5 questions highscore chart will appear
-  });
+// all clicks
+
+//  start button
+  var startquizbutton = document.querySelector("#button");
+  startquizbutton.addEventListener("click", function(event) {
+      console.log("startquiz")
+
+  // button disapears, 
+  // quiz starts-- see first questions & answers, 
+  // timer starts, 
+  // clicks an a answer/button then goes to next
+  // end of 5 questions highscore chart will appear
+    });
+
+
 
 // storing questions & answers
 // 5 questions that will appear with answers
