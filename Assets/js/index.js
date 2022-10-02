ef775a398fcc8600ff6a0ade3e16efde70c57721
Calvin Tran <caltran@Calvins-MBP.fios-router.home>
2022-10-02 17:50:35 -0400

    added questions

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -23,10 +23,13 @@ clicks ();
 
 // storing questions & answers
 // 5 questions that will appear with answers
-function QuestionsAnswers(){
-var Questions = [
+function QuestionsAnswers()
+{
+var Questions = 
+[
 { question1: "Commonly used data types Do not include:",
-Answers: {
+Answers: 
+{
   1: 'strings',
   2: 'boolens',
   3: 'alerts',
@@ -34,15 +37,48 @@ Answers: {
 }, 
   correctAnswer: '3'
 },
-{ questions2: "The condition in a if/else statement is enclosed with________.",
-Answers: {
+{ question2: "The condition in a if/else statement is enclosed with________.",
+Answers: 
+{
   1: 'quotes',
   2: 'curly brackets',
   3: 'parenthesis',
   4: 'square brackets',
-}}
-] 
+},
+  correctAnswer: '2'
+},
+{ question3: "arrays in Javascript can be used to store ________.",
+Answers: 
+{
+  1: 'numbers & strings',
+  2: 'other arrays',
+  3: 'booleans',
+  4: 'all of the above',
+},
+  correctAnswer: '1'
+},
+{ question4: "string values must be enclosed within_______when being assigned to variables.",
+Answers: 
+{
+  1: 'commas',
+  2: 'curly brackets',
+  3: 'quotes',
+  4: 'parenthesis',
+},
+  correctAnswer: '4'
+},
+{ question5: "a very useful tool during development and debugging is:",
+Answers:
+{
+  1: 'Javascript',
+  2: 'terminal/bash',
+  3: 'for loops',
+  4: 'console.log',
+},
+  correctAnswer: '4'
+
 }
+]}
 
 // storing scores 
 // highscore chart ranks scores for high to low
